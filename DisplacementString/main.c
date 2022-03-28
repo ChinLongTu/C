be0b107bae8fbfb9f88bea327cbe3a8c59e76fde
FredTu <92671023+ChinLongTu@users.noreply.github.com>
2022-03-28 18:15:51 +0800

    Add files via upload

--- DisplacementString/main.c
+++ DisplacementString/main.c
@@ -1,54 +1,47 @@
 #include <stdio.h>
 
-void displacement(char str[],int offset);
+void displacementStr(char str[],int offset);
 int main(int argc, char *argv[])
 {
-
   char str[500];
   int offset;
   
   printf("請輸入一個字串: "); 
   scanf("%s", str);
-  // printf("\n");
   printf("請輸入偏移量: ");
   scanf("%d", &offset);
-  displacement(str,offset);
-
-  // printf("\n");
-  // printf("字串: %s\n",str);
-  // printf("偏移量: %d",offset);
+  displacementStr(str,offset);
   
   
   return 0;
 }
 
-void displacement(char str[],int offset){
+void displacementStr(char str[],int offset){
 	int i=0,j=0;
   int newC=0;
+  //偏移量校正
+  while(offset > 95 || offset < -95){
+    if(offset > 95){
+      offset -= 95;
+    }else{
+      offset += 95;
+    }
+  }
+  //讀取字元數量
 	while(str[i]!='\0'){
 		i++;
 	}
 	printf("轉換後的字串: ");
+  //字元各別轉換
 	for(j=0;j<i;j++){
-  //   printf("org = %d\n",str[j]);
-  //   str[j] = str[j] + offset;
-  //   if(str[j]>126){
-  //     str[j] = str[j] % 95;
-  //   }
-		// printf("new = %d\n",str[j]);
-    printf("org = %d\n",str[j]);
-    offset = offset % 95;
     newC = str[j] + offset;
-    printf("newC = %d\n",newC);
     if(newC > 126){
-      newC = newC % 95;
+      newC = newC - 95;
     }
-    printf("newCmod95 = %d\n",newC);
     if(newC < 32){
       newC = newC + 95;
     }
-    printf("newCadd95 = %d\n",newC);
-    printf("new = %c\n",newC);
+    printf("%c",newC);
 	}
 	printf("\n");
 }
