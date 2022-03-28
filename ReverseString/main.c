f7f2ec5e9b01f6fa8e59c921b7ae98aed78c260d
FredTu <92671023+ChinLongTu@users.noreply.github.com>
2022-03-28 18:17:07 +0800

    Add files via upload

--- ReverseString/main.c
+++ ReverseString/main.c
@@ -1,25 +1,25 @@
 #include <stdio.h>
 
-void reverse(char str[]);
+void reverseString(char str[]);
 int main(int argc, char *argv[])
 {
-
   char str[500];
   
   printf("請輸入一個字串: "); 
   scanf("%s", str);
-  reverse(str);
+  reverseString(str);
   
   return 0;
 }
 
-void reverse(char str[]){
+void reverseString(char str[]){
 	int i=0,j=0;
 	while(str[i]!='\0'){
 		i++;
 	}
 	printf("轉換後的字串: ");
-	for(j=i-1;j>=0;j--){
+	//字元顛倒取出
+  for(j=i-1;j>=0;j--){
 		printf("%c",str[j]);
 	}
 	printf("\n");
